455d8611aece99367ce255a53f35a79d40403889
hftamayo <hftamayo@gmail.com>
2021-05-03 07:46:54 -0600

    final details of todoapp

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from '../components/Persons/Person/Person'
+import Todo from './Todo'
 
 
-class App extends Component() {
-  state = {
-    persons: [
-      {id: '001', name: 'Lionel Messi', age: 34},
-      {id: '002', name: 'Ousmane Dembele', age: 21},      
-      {id: '003', name: 'Ansu Fati', age: 18},      
-    ],
-    otherState: 'waiting for more lineups',
-    showPersons: false
-  }
+function App(){
+  return(
+    <div>
+      <Todo />
+    </div>
 
-  nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-  }
-
-  const person = {
-    ...this.state.person[personIndex]
-  }
-
+  );
 }
 export default App;
