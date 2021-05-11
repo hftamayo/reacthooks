68ebec0381d45a2bb8177f859696acc06d8268a4
hftamayo <hftamayo@gmail.com>
2021-05-11 15:48:36 -0600

    less32: adding first component

---
+++ src/containers/Expenses.js
@@ -0,0 +1,10 @@
+import ExpenseItem from './components/Expenses';
+
+function Todo(){
+    return(
+        <div>
+            <h1>My expenses</h1>
+            <ExpenseItem />
+        </div>
+    );
+}
