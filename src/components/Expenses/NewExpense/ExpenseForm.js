699a677fa4e49210a16877c605952c11c715ebed
hftamayo <hftamayo@gmail.com>
2021-05-27 11:59:18 -0600

    less 52: listtening user input

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const titleChangeHandler = (event) => { 
+        //imprimir conforme el usuario escribe
+        console.log(event.target.value);
+
+    };
+
+
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text"
+                    onChange={titleChangeHandler} />
+
+
+
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
