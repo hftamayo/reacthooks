a6fd88146ca02f4986185e5c0d7bbbfbbf685dbe
hftamayo <hftamayo@gmail.com>
2021-06-18 11:32:40 -0600

    rendering conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,9 +8,26 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log("Expenses.js received this value: ");
-    console.log(selectedYear);
   };
+
+  //solucion de Max al assignment 3
+  const filteredExpense = props.records.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if(filteredExpense.length > 0) {
+    expensesContent = 
+      filteredExpense.map((expense) => (
+        <ExpenseItemV3
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+      ));
+  }
 
   return (
     <div>
@@ -19,6 +36,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {expensesContent}
+        {/* mi solucion al assignment 3
         {props.records
           .filter((expense) => expense.date.getFullYear().toString() === filteredYear)
           .map((filteredExpense) => (
@@ -28,7 +47,8 @@ const Expenses = (props) => {
               amount={filteredExpense.amount}
               date={filteredExpense.date}
             />
-          ))}
+          ))}        
+        */}
       </Card>
     </div>
   );
