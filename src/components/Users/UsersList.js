215379e7086e9a32376cb424c6d9b86525c62360
hftamayo <hftamayo@gmail.com>
2021-07-12 13:07:07 -0600

    fixing bug on display users

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,16 +3,19 @@ import CardUsers from "../UI/CardUsers";
 import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
+  const NoData = <li>No users to display</li>;
   const CurrentUsersList = props.users;
+
   return (
     <CardUsers className={classes.users}>
       <ul>
-        {!CurrentUsersList.length &&
-          CurrentUsersList.map((user) => (
-            <li key={user.id}>
-              {user.name} ({user.age} years old)
-            </li>
-          ))}
+        {CurrentUsersList.length === 0
+          ? NoData
+          : CurrentUsersList.map((user) => (
+              <li key={user.id}>
+                {user.name} ({user.age} years old)
+              </li>
+            ))}
       </ul>
     </CardUsers>
   );
