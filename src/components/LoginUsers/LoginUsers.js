1b860dc4f632ee0ad2e551446387b5766ea9c757
hftamayo <hftamayo@gmail.com>
2021-07-13 18:07:00 -0600

    using useEffect in login component

--- src/components/LoginUsers/LoginUsers.js
+++ src/components/LoginUsers/LoginUsers.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './Login/Login';
-import Home from './Home/Home';
-import MainHeader from './MainHeader/MainHeader';
+import Login from "./Login/Login";
+import Home from "./Home/Home";
+import MainHeader from "./MainHeader/MainHeader";
 
 function LoginUsers() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    //sin el uso de useEffect, el chequeo de esta flag sería infinito
+    //la funcion es ejecutada DESPUES que el componente ha sido re-evaluado
+    const storeUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storeUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
