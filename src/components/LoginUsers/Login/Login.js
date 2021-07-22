6b2bbf4aac88287711f997760b1f9ce17ca715c4
hftamayo <hftamayo@gmail.com>
2021-07-22 17:05:26 -0600

    refactoring input component

--- src/components/LoginUsers/Login/Login.js
+++ src/components/LoginUsers/Login/Login.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useReducer, useContext } from "react";
 import Card from "../../UI/Card";
 import classes from "./Login.module.css";
 import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
 import AuthContext from "../store/auth-context";
 
 const emailReducer = (state, action) => {
@@ -97,29 +98,25 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
-          <label htmlFor="password">Password</label>
-          <input
+          <Input
+            id="paswword"
+            label="Password"
             type="password"
-            id="password"
+            isValid={passwordIsValid}
             value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
