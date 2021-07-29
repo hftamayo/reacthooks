b605eb5a26767d16585dddfa710267a7b3a81c89
hftamayo <hftamayo@gmail.com>
2021-07-29 17:32:13 -0600

    adding meals component

--- src/components/FoodOrder/FoodOrder.js
+++ src/components/FoodOrder/FoodOrder.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import Header from './Layout/Header';
+import Meals from './Meals/Meals';
 
 
 const VerGoals = () => {
     return(
         <Fragment>
             <Header />
+            <main>
+                <Meals />
+            </main>
         </Fragment>
 
     );
