244610d6df9bf96efb8341df2d4128f92a8fe200
hftamayo <hftamayo@gmail.com>
2021-08-04 19:58:09 -0600

    adding modal form

--- src/components/FoodOrder/Meals/Meals.js
+++ src/components/FoodOrder/Meals/Meals.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
+import Cart from '../Cart/Cart';
 
 const Meals = () => {
     return(
         <Fragment>
+            <Cart />
             <MealsSummary />
             <AvailableMeals />
         </Fragment>
---
+++ src/components/FoodOrder/UI/Modal.js
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.Backdrop} />;
+};
+
+const ModalOverLay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />)}
+      {ReactDOM.createPortal(<ModalOverLay>{props.children}</ModalOverLay>)}
+      {portalElement}
+    </Fragment>
+  );
+};
+
+export default Modal;
