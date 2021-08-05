322fdca35ca7e387ad7b78267bf2526fdb939e44
hftamayo <hftamayo@gmail.com>
2021-08-04 18:53:44 -0600

    adding a form

---
+++ src/components/FoodOrder/UI/Input.js
@@ -0,0 +1,15 @@
+import classes from './Input.module.css';
+
+const Input = (props) => {
+    return(
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            {/* el spread operator es para estar seguro que el
+            ultimo valor es qel que se utilizara y que interactua con
+            los demas componentes padres */}
+            <input id={props.input.id} {...props.input}/>
+        </div>
+    );
+}
+
+export default Input;
