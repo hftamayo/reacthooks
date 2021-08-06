90add9d45e92a0c54e7ef97f5136eb766ddd580f
hftamayo <hftamayo@gmail.com>
2021-08-06 13:40:12 -0600

    cart reducer, refs and fwd refs part I

--- src/components/FoodOrder/store/CartProvider.js
+++ src/components/FoodOrder/store/CartProvider.js
@@ -1,23 +1,48 @@
-import Cart from '../Cart/Cart';
-import CartContext from './cart-context';
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
-const CartProvider = props => {
-    const addItemToCartHandler = item => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-    const removeItemFromCartHandler = id => {};
+//en una funcion reducer siempre tenemos los 2 parametros:
+//state y action
+const cartReducer = (state, action) => {
+    if(action.type == 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+  return defaultCartState;
+};
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    };
+const CartProvider = (props) => {
+const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    return(
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    );
-}
+  const addItemToCartHandler = (item) => {
+      dispatchCartAction({type: 'ADD', item: item});
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type: 'REMOVE', id: id});
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
 export default CartProvider;
