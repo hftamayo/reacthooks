1367d0f4994f32c8e3a9c9e6d70b7e869446552f
hftamayo <hftamayo@gmail.com>
2021-08-13 16:53:12 -0600

    using async/await to fecth data from api

--- src/components/Movies/VerMovies.js
+++ src/components/Movies/VerMovies.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MovieList from './MovieList';
-import './VerMovies.module.css';
+import MovieList from "./MovieList";
+import "./VerMovies.module.css";
 
 function VerMovies() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={dummyMovies} />
+        <MovieList movies={movies} />
       </section>
     </React.Fragment>
   );
