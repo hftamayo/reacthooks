94611be013f3888138a8cacf95907ac7ecb79b7b
hftamayo <hftamayo@gmail.com>
2021-08-25 17:03:13 -0600

    bug on use-http hook implementation

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,39 +1,38 @@
-import React, { useState } from 'react';
+import { useState, useCallBack } from "react";
 
-const useHttp = (requestConfig, applyData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const sendRequest = async (taskText) => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-            requestConfig.url, {
-                method: requestConfig.method ? requestConfig.method : 'GET',
-                headers: requestConfig.headers ? requestConfig.method: {},
-                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-            }
-        );
-  
-        if (!response.ok) {
-          throw new Error('Request failed!');
-        }
-  
-        const data = await response.json();
-        applyData(data);
-    } catch (err) {
-        setError(err.message || 'Something went wrong!');
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallBack(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.method : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
       }
-      setIsLoading(false);
-    };
-    return {
-        /*ES6 notation: same localvars equals to another objects
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+  
+  return {
+    /*ES6 notation: same localvars equals to another objects
         isLoading: isLoading */
-        isLoading,
-        error,
-        sendRequest,
-    };
+    isLoading,
+    error,
+    sendRequest,
+  };
 };
 
 export default useHttp;
