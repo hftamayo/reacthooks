1ecb8e1bf2935be88af384d5784c210f72691c36
hftamayo <hftamayo@gmail.com>
2021-08-26 13:37:31 -0600

    fixing bug on use-http

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,15 +1,15 @@
-import { useState, useCallBack } from "react";
+import { useState, useCallback } from "react";
 
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = useCallBack(async (requestConfig, applyData) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
+        method: requestConfig.method ? requestConfig.method : 'GET',
         headers: requestConfig.headers ? requestConfig.method : {},
         body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
@@ -28,7 +28,10 @@ const useHttp = () => {
   
   return {
     /*ES6 notation: same localvars equals to another objects
-        isLoading: isLoading */
+        isLoading: isLoading 
+        */
+
+
     isLoading,
     error,
     sendRequest,
@@ -36,3 +39,44 @@ const useHttp = () => {
 };
 
 export default useHttp;
+
+
+//import { useState, useCallback } from 'react';
+/*
+import { useState, useCallback } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
+*/
