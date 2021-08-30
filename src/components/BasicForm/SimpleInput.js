267761e84fd7bdca03f64ff7640d6e31afb8a2e3
hftamayo <hftamayo@gmail.com>
2021-08-30 10:39:26 -0600

    capturing input after submit

--- src/components/BasicForm/SimpleInput.js
+++ src/components/BasicForm/SimpleInput.js
@@ -1,16 +1,28 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
-    return (
-      <form>
-        <div className='form-control'>
-          <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className="form-actions">
-          <button>Submit</button>
-        </div>
-      </form>
-    );
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
   };
-  
-  export default SimpleInput;
-  
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
