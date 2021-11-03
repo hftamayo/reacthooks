d3efe16bf130ec3c3946c0cfdaa468873d867678
hftamayo <hftamayo@gmail.com>
2021-11-02 20:52:30 -0600

    applying hooks to a new form p1

--- src/components/BasicForm/BasicForm.js
+++ src/components/BasicForm/BasicForm.js
@@ -1,25 +1,72 @@
+import useInput from "./hooks/use-input";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 const BasicForm = (props) => {
-    return (
-      <form>
-        <div className='control-group'>
-          <div className='form-control'>
-            <label htmlFor='fname'>First Name</label>
-            <input type='text' id='fname' />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='lname'>Last Name</label>
-            <input type='text' id='lname' />
-          </div>
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  return (
+    <form>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={firstNameValue}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='usremail'>E-Mail Address</label>
-          <input type='text' id='usremail' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
         </div>
-        <div className='form-actions'>
-          <button>Submit</button>
-        </div>
-      </form>
-    );
-  };
-  
-  export default BasicForm;
+      </div>
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicForm;
