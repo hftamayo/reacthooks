9ac26cf51720092d944ba144df4c65872d67e046
hftamayo <hftamayo@gmail.com>
2021-11-05 10:24:28 -0600

    providing redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import { AuthContextProvider } from './components/LoginUsers/store/auth-context';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 ReactDOM.render(
-  <AuthContextProvider>
+  <Provider store={store}>
     <App />
-  </AuthContextProvider>,
+  </Provider>,
   document.getElementById('root')
 );
 
