8af6eaa93d5e4f8c21ba0646b6069d9572705c81
hftamayo <hftamayo@gmail.com>
2021-11-10 09:51:46 -0600

    working with states

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment'){
         return{
             counter: state.counter + 1,
-        }
+            showCounter: state.showCounter
+        };
     }
 
     if(action.type === 'increase'){
@@ -12,15 +15,25 @@ const counterReducer = (state = { counter: 0 }, action) => {
             //usar este payload no es eficiente
             //counter: state.counter + 5,
             counter: state.counter + action.amount,
+            showCounter: state.showCounter
 
-        }
+        };
     }
 
     if(action.type === 'decrement'){
         return{
             counter: state.counter - 1,
-        }
+            showCounter: state.showCounter
+        };
     }    
+
+    if(action.type === 'toggle'){
+        return{
+            showCounter: !state.showCounter,
+            counter: state.counter
+
+        };
+    }
 
     return state;
 };
