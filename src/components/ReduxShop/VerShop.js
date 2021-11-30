1ff7d559160d8944545e7977a8283c2c912c7361
hftamayo <hftamayo@gmail.com>
2021-11-30 10:50:45 -0600

    using redux thunk

--- src/components/ReduxShop/VerShop.js
+++ src/components/ReduxShop/VerShop.js
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./Cart/Cart";
 import Layout from "./Layout/Layout";
 import Products from "./Shop/Products";
-import { uiActions } from "../../storeshop/ui-slice";
 import Notification from "./UI/Notification";
+import {sendCartData} from "../../storeshop/cart-slice";
 
 let isInitial = true;
 
@@ -16,47 +16,11 @@ function VerShop() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = fetch(
-        "https://movieserp-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if(isInitial){
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
