4f390a14b42aac2c7af087acd05a2701ec21a21e
hftamayo <hftamayo@gmail.com>
2021-12-16 11:48:07 -0600

    nested routes part2

--- src/components/Quotes/VerQuotes.js
+++ src/components/Quotes/VerQuotes.js
@@ -1,27 +1,24 @@
-import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
 import AllQuotes from "./AllQuotes";
 import NewQuote from "./NewQuote";
 import QuoteDetail from "./QuoteDetail";
 
 function VerQuotes() {
+  const { path, url } = useRouteMatch();
   return (
-    <BrowserRouter>
       <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-          {/* si no pongo exact no se renderiza la segunda opcion */}
-        <Route path="/quotes" exact>
+        <Route path={`${path}/quotes/:quoteId`}>
+          <QuoteDetail />
+        </Route>        
+        {/* si no pongo exact no se renderiza la segunda opcion */}
+        <Route path={`${path}/quotes`}>
           <AllQuotes />
         </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
+        <Route path={`${path}/new-quote`}>
           <NewQuote />
         </Route>
+        <Redirect from={path} to={`${path}/quotes`} />
       </Switch>
-    </BrowserRouter>
   );
 };
 
