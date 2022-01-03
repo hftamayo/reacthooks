b40d662246ce748e3faa658585a81c16684215a9
hftamayo <hftamayo@gmail.com>
2022-01-03 14:58:01 -0600

    adding flexibility to routing

--- src/components/Quotes/QuoteDetail.js
+++ src/components/Quotes/QuoteDetail.js
@@ -1,35 +1,48 @@
-import { Fragment } from 'react';
-import { useParams, Route, useRouteMatch } from 'react-router-dom';
+import { Fragment } from "react";
+import { useParams, Route, useRouteMatch, Link } from "react-router-dom";
 
-import HighlightedQuote from './components/HighlightedQuote';
-import Comments from './Comments/Comments';
+import HighlightedQuote from "./components/HighlightedQuote";
+import Comments from "./Comments/Comments";
 
 const DUMMY_NOTES = [
-    {
-        id: 'q1', author: 'Max', text: 'Learning React is fun'
-    },
-    {
-        id: 'q2', author: 'Seb', text: 'Learning Node is great'
-    },    
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun",
+  },
+  {
+    id: "q2",
+    author: "Seb",
+    text: "Learning Node is great",
+  },
 ];
 
 const QuoteDetail = () => {
-    const { path, url } = useRouteMatch();
-    const params = useParams();
+  const match = useRouteMatch();
+  const params = useParams();
 
-    const quote = DUMMY_NOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_NOTES.find((quote) => quote.id === params.quoteId);
 
-    if(!quote){
-        return <p>No Quote Found!</p>;
-    }
-    return(
-        <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author} />
-            <Route path={`${path}/quotes/${params.quoteId}/comments`}></Route>
-            <Comments />
-        </Fragment>
-
-    );
+  if (!quote) {
+    return <p>No Quote Found!</p>;
+  }
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link
+            className="btn--flat"
+            to={`${match.url}/comments`}
+          >
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}></Route>
+      <Comments />
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
--- src/components/Quotes/components/QuoteList.js
+++ src/components/Quotes/components/QuoteList.js
@@ -17,7 +17,7 @@ const sortQuotes = (quotes, ascending) => {
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
-  const { path } = useRouteMatch();  
+  const  match  = useRouteMatch();  
 
   const queryParams = new URLSearchParams(location.search);
   const isSortingAscending = queryParams.get("sort") === "asc";
@@ -25,7 +25,7 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push(`${path}?sort=` + (isSortingAscending ? 'desc' : 'asc'));
+    history.push(`${match.path}?sort=${(isSortingAscending ? 'desc' : 'asc')}`);
   };
 
   return (
