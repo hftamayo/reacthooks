828194e5871198afbf3932fd2a8f7a081f368cfd
hftamayo <hftamayo@gmail.com>
2022-02-04 14:21:57 -0600

    preparing application for deployment

--- src/components/Quotes/VerQuotes.js
+++ src/components/Quotes/VerQuotes.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import AllQuotes from "./AllQuotes";
-import NewQuote from "./NewQuote";
-import QuoteDetail from "./QuoteDetail";
 import Layout from "./layout/Layout";
+import LoadingSpinner from "./UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./NewQuote"));
+const QuoteDetail = React.lazy(() => import("./QuoteDetail"));
+const NotFound = React.lazy(() => import("./NotFound"));
+const AllQuotes = React.lazy(() => import("./AllQuotes"));
 
 const VerQuotes = () => {
   return (
     <div>
       <main>
         <Layout>
-          <Routes>
-            <Route path="quotes" element={<AllQuotes />} />
-
-            <Route path="new-quote" element={<NewQuote />} />
-
-            <Route path="quotes/:quoteId" element={<QuoteDetail />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="centered">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="quotes" element={<AllQuotes />} />
+              <Route path="new-quote" element={<NewQuote />} />
+              <Route path="quotes/:quoteId" element={<QuoteDetail />} />
+            </Routes>
+          </Suspense>
         </Layout>
       </main>
     </div>
